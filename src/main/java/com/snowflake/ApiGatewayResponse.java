0a90de85c7ed3ac080517575d4c4eee4bb9c4b59
Brad <bradculberson@gmail.com>
2021-11-12 13:27:05 -0700

    format cleanup

--- src/main/java/com/snowflake/ApiGatewayResponse.java
+++ src/main/java/com/snowflake/ApiGatewayResponse.java
@@ -77,8 +77,8 @@ public class ApiGatewayResponse {
 		}
 
 		/**
-		 * Builds the {@link ApiGatewayResponse} using the passed object body
-		 * converted to JSON.
+		 * Builds the {@link ApiGatewayResponse} using the passed object body converted
+		 * to JSON.
 		 */
 		public Builder setObjectBody(Object objectBody) {
 			this.objectBody = objectBody;
@@ -86,9 +86,9 @@ public class ApiGatewayResponse {
 		}
 
 		/**
-		 * Builds the {@link ApiGatewayResponse} using the passed binary body
-		 * encoded as base64. {@link #setBase64Encoded(boolean)
-		 * setBase64Encoded(true)} will be in invoked automatically.
+		 * Builds the {@link ApiGatewayResponse} using the passed binary body encoded as
+		 * base64. {@link #setBase64Encoded(boolean) setBase64Encoded(true)} will be in
+		 * invoked automatically.
 		 */
 		public Builder setBinaryBody(byte[] binaryBody) {
 			this.binaryBody = binaryBody;
@@ -100,8 +100,7 @@ public class ApiGatewayResponse {
 		 * A binary or rather a base64encoded responses requires
 		 * <ol>
 		 * <li>"Binary Media Types" to be configured in API Gateway
-		 * <li>a request with an "Accept" header set to one of the "Binary Media
-		 * Types"
+		 * <li>a request with an "Accept" header set to one of the "Binary Media Types"
 		 * </ol>
 		 */
 		public Builder setBase64Encoded(boolean base64Encoded) {
